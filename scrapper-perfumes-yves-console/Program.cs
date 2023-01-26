db6f3b3fd785cf78dcbf2066e8f18cfab4436723
claudiopedalino <claudio.dpedalino@gmail.com>
2023-01-25 23:15:46 -0300

    get url and items

--- scrapper-perfumes-yves-console/Program.cs
+++ scrapper-perfumes-yves-console/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Reflection;
+
+var dirverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+using (IWebDriver driver = new ChromeDriver(dirverPath))
+{
+    //Setup Driver
+    driver.Navigate().GoToUrl("https://yvesdorgeval.empretienda.com.ar/perfumes/perfumes-femeninos");
+    driver.Manage().Window.Maximize();
+
+    // 01. Force load the whole data
+    var btn = driver.FindElement(By.Id("products_feed-btn"));
+
+    while (btn.Enabled && btn.Displayed)
+    //for (int i = 0; i < 2; i++) 
+    {
+        Thread.Sleep(1200);
+        btn.Click();
+        Thread.Sleep(1200);
+    }
+
+    // 02. Get items list
+    var items = driver.FindElements(By.ClassName("products-feed__product-wrapper"));
+    global::System.Console.WriteLine(   items.Count);
+
+
+    //Close Driver
+    driver.Quit();
+    Environment.Exit(0);
+}
+
+Console.WriteLine("End!");
